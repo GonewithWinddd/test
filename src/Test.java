5561653d367880d6193bed06c2d81ab90f042340
Gone <mrge1018>
2019-11-18 14:46:01 +0800

    java8filter

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("测试1.6");
         System.out.println("测试上传的效果");
+
+        List<String> list = new ArrayList<String>();
+        list.add("数学");list.add("yy");
+        List<String> res =list.stream().filter(x->x.equals("yy")).collect(Collectors.toList());
+        System.out.println(res.size());
     }
 }
